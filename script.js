6e116954b61e4c1563d2c906d8fe352ae4f18481
StefanyvitoriavillaAltaSilva <111292816+StefanyvitoriavillaAltaSilva@users.noreply.github.com>
2024-06-05 09:45:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const caixaPrincipal = document.querySelector("caixa-principal")
+
+const caixaPerguntas = document.querySelector("caixa-perguntas")
+
+const caixaAlternativas = document.querySelector("caixa-alternativas")
+
+const caixaResultados = document.querySelector("caixa-resultados")
+
+const caixaResultados = document.querySelector("caixa-resultados")
+
+const perguntas = [
+{
+         enunciado: 'pergunta1'
+     alternativas:
+
+}
+
+
+
+
+]
